eb9518f29c39b02faf56eca68a366364555f258b
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-06-25 17:57:55 +0000

    Added repack to admin
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@28514 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -5,7 +5,7 @@
 class Admin_Controller extends ORM_Manager_Controller
 {
     protected $url_base = 'admin';
-    protected $known_models = array('post', 'profile', 'login');
+    protected $known_models = array('post', 'profile', 'login', 'repack');
 
     /**
      * Initialize the controller.
