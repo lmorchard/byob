dbfb7ec9ff42bfaf491aa9faa5db25eec4b46314
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-06-30 03:49:20 +0000

    Adding requires for Zend vendor modules in auto_profiles init hook
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@29021 4eb1ac78-321c-0410-a911-ec516a8615a5

--- modules/auth_profiles/hooks/init.php
+++ modules/auth_profiles/hooks/init.php
@@ -12,5 +12,17 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path()
 )));
 
+require_once('BigOrNot/CookieManager.php');
+require_once('Zend/Acl/Assert/Interface.php');
+require_once('Zend/Acl/Exception.php');
+require_once('Zend/Acl/Resource/Interface.php');
+require_once('Zend/Acl/Resource.php');
+require_once('Zend/Acl/Role/Interface.php');
+require_once('Zend/Acl/Role/Registry/Exception.php');
+require_once('Zend/Acl/Role/Registry.php');
+require_once('Zend/Acl/Role.php');
+require_once('Zend/Acl.php');
+require_once('Zend/Exception.php');
+
 // Initialize the cookie handler for login.
 authprofiles::init();
