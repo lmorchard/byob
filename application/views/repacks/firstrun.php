c4e768c70cd38084e2bde407d3b243da2cc0fb0f
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-07-13 23:37:02 +0000

    bug 503326: added links to addons
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@29699 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/firstrun.php
+++ application/views/repacks/firstrun.php
@@ -14,13 +14,14 @@
         <?php foreach ($repack->collection_addons as $addon): ?>
             <?php
                 $h = html::escape_array(array(
+                    'id'      => $addon->id,
                     'icon'    => $addon->icon,
                     'name'    => $addon->name,
                     'summary' => $addon->summary,
                 ));
             ?>
             <li class="addon">
-                <img src="<?=$h['icon']?>" /> <?=$h['name']?>
+                <a href="https://addons.mozilla.org/en-US/firefox/addon/<?=$h['id']?>" target="_new"><img src="<?=$h['icon']?>" /> <?=$h['name']?></a>
                 <p><?=$h['summary']?></p>
             </li>
         <?php endforeach ?>
