dd93f1eaba86afffb7a270487dadbc2269436f15
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-08-13 21:49:21 +0000

    bug 510314: update forgotten password email text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@49153 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/auth_profiles/forgotpassword_email.php
+++ application/views/auth_profiles/forgotpassword_email.php
@@ -1,12 +1,30 @@
-From: l.m.orchard@gmail.com
-Subject: Password recovery for <?=$login_name?>.
+From: byob-notify-noreply@mozilla.com
+Subject: Build Your Own Browser: Password reset for <?=$login_name?>
 
-Someone (possibly you) has triggered an attempt to recover the password for a 
-login named "<?=$login_name?>" registered with this email address.  If you are 
-that someone, follow this link to complete the process:
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account of this email is not monitored, so 
+please direct enquires about this email to the BYOB contact page at 
+<?=$contact_URL?>.
+
+Someone (possibly you) has requested a reset of the password for the BYOB login 
+named "<?=$login_name?>". This login was registered with this email address. If 
+you are that someone, and wish to reset your password, please follow the link 
+below to complete the password reset process:
+ 
 <?= 
 url::full('changepassword', false, array(
     'password_reset_token' => $password_reset_token
-))
+)) . "\n"
 ?>
+
+If you did not request a password reset for this account, simply ignore this 
+email, and your current password will remain in place. If you have any 
+questions about this email, or experience difficulties resetting your password, 
+please contact us via the BYOB contact page at <?=$contact_URL?>.
+
+Thanks!
+
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
