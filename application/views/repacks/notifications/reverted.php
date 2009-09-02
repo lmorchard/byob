e191b87bbfd116494138aa3a71940fa3366642ac
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:08:51 +0000

    bug 514165: Modify reverted repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50690 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/reverted.php
+++ application/views/repacks/notifications/reverted.php
@@ -1,8 +1,18 @@
-From: admin@byob.mozilla.com
+From: byob-notify-noreply@mozilla.com
 Subject: [BYOB] Release take down for <?=$repack->display_title?> 
 
-A release has been taken down for browser <?=$repack->display_title?>.
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>.  
+
+
+The current release for browser <?=$repack->display_title?> has been removed at 
+your request. This browser is no longer available for download. If you want to 
+re-release this version of the browser, you must re-submit it for release.
+
+Status information and the release history of this browser can be found at:
 <?=$repack->url() . "\n" ?>
 
 <?php if (!empty($comments)): ?>
@@ -13,3 +23,5 @@ Comments:
 
 <?php endif ?>
 
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
