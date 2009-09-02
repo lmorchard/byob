76775e66d8d4c2a758c5857dcbc240d24e5f6a17
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:47:45 +0000

    bug 514171: Modify failed repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50700 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/failed.php
+++ application/views/repacks/notifications/failed.php
@@ -1,8 +1,18 @@
-From: admin@byob.mozilla.com
-Subject: [BYOB] Release failed for <?=$repack->display_title?> 
+From: byob-notify-noreply@mozilla.com
+Subject: [BYOB] Release failed for <?=$repack->display_title . "\n"?>
 
-A requested release for the browser <?=$repack->display_title?> has failed.
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>.
+
+The generation of a requested release for the browser 
+<?=$repack->display_title?> has failed. BYOB administrators have been notified, 
+and you must re-submit the release for generation and approval. Our apologies 
+for the inconvenience this may cause.
+
+Status information and the release history of this browser can be found at:
 <?=$repack->url() . "\n" ?>
 
 <?php if (!empty($comments)): ?>
@@ -12,3 +22,6 @@ Comments:
 <?=$comments?>
 
 <?php endif ?>
+
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
