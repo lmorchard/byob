b2da29e86046581d144e4a33ad0f1040fc04cf41
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:35:23 +0000

    bug 514172: Modify cancelled repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50696 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/cancelled.php
+++ application/views/repacks/notifications/cancelled.php
@@ -1,9 +1,20 @@
-From: admin@byob.mozilla.com
-Subject: [BYOB] Release cancelled for <?=$repack->display_title?> 
+From: byob-notify-noreply@mozilla.com
+Subject: [BYOB] Release cancelled for <?=$repack->display_title?>
 
-A requested release for the browser <?=$repack->display_title?> has been cancelled.
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>.
+
+
+A requested release for the browser <?=$repack->display_title?> has been 
+cancelled, and the release has been removed from the review queue. If this 
+release was cancelled in error, you must re-submit the release for approval.
+
+Status information and the release history of this browser can be found at:
 <?=$repack->url() . "\n" ?>
+
 <?php if (!empty($comments)): ?>
 
 Comments:
@@ -11,3 +22,7 @@ Comments:
 <?=$comments?>
 
 <?php endif ?>
+
+
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
