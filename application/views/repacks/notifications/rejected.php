2ff892c166318623b8fd252722b87efb46906dcf
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:46:03 +0000

    bug 514169: Modify rejected repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50699 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/rejected.php
+++ application/views/repacks/notifications/rejected.php
@@ -1,8 +1,20 @@
-From: admin@byob.mozilla.com
-Subject: [BYOB] Release rejected for <?=$repack->display_title?> 
+From: byob-notify-noreply@mozilla.com
+Subject: [BYOB] Release rejected for <?=$repack->display_title . "\n"?>
 
-The release requested for browser <?=$repack->display_title?> has been rejected.
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>. 
+
+
+The release requested for browser <?=$repack->display_title?> has been 
+rejected. Please review the comments below for information on why the release 
+was rejected, and modify the customizations accordingly. You will need to 
+re-submit your release request once the changes have been made. If you have any 
+questions regarding the comments below, please use the contact form (above).
+
+Status information and the release history of this browser can be found at:
 <?=$repack->url() . "\n" ?>
 
 <?php if (!empty($comments)): ?>
@@ -13,3 +25,5 @@ Comments:
 
 <?php endif ?>
 
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
