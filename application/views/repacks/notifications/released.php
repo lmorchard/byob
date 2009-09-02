d91a10a32bc15544aa2cafa9cce6d3fa6cecdf2f
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:38:21 +0000

    bug 514168: Modify released repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50697 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/released.php
+++ application/views/repacks/notifications/released.php
@@ -1,8 +1,19 @@
-From: admin@byob.mozilla.com
-Subject: [BYOB] Release approved for <?=$repack->display_title?> 
+From: byob-notify-noreply@mozilla.com
+Subject: [BYOB] Release approved for <?=$repack->display_title?>
 
-The release requested for browser <?=$repack->display_title?> has been approved.
+Greetings,
 
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>. 
+
+
+The release requested for browser <?=$repack->display_title?> has been 
+approved, and the installer files are now available for download from your BYOB 
+profile. You may now distribute this release under the conditions outlined when 
+you submitted the release for approval.
+
+Status information and the release history of this browser can be found at:
 <?=$repack->releaseUrl() . "\n" ?>
 
 <?php if (!empty($comments)): ?>
@@ -13,3 +24,5 @@ Comments:
 
 <?php endif ?>
 
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
