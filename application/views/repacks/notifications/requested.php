1b6e4cb9a6c251b358ec6880f009e9dbd6cb7f0c
lorchard@mozilla.com <lorchard@mozilla.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-09-02 16:22:04 +0000

    bug 514166: Modify requested repack notification text
    git-svn-id: https://svn.mozilla.org/projects/byob/trunk@50692 4eb1ac78-321c-0410-a911-ec516a8615a5

--- application/views/repacks/notifications/requested.php
+++ application/views/repacks/notifications/requested.php
@@ -1,7 +1,25 @@
-From: admin@byob.mozilla.com
-Subject: [BYOB] Release requested for <?=$repack->display_title?> 
+From: byob-notify-noreply@mozilla.com
+Subject: [BYOB] Release requested for <?=$repack->display_title?>
 
-A release has been requested for browser <?=$repack->display_title?>.
+Greetings,
+
+This is an automatically generated email from Mozilla's Build Your Own Browser 
+(BYOB) application. The orignating account is not monitored, so please direct 
+enquires about this email to the BYOB contact page at <?=$contact_URL?>. 
+
+
+The request for release of the <?=$repack->display_title?> browser has been 
+succesfully submitted. Your release will now be generated and submitted for 
+review. The installer files will not be available for download until the review 
+process has completed, and any change requests will be sent to this email 
+address. You will receive additional notifications for when the release has 
+been generated for review, and when the review is complete.
+
+Please note that our turnaround target for review is two (2) business days, and 
+be greater or less depending on the number of release requests currently being 
+reviewed.
+
+Status information and the release history of this browser can be found at:
 
 <?=$repack->url() . "\n" ?>
 
@@ -13,3 +31,5 @@ Comments:
 
 <?php endif ?>
 
+The Mozilla Build Your Own Browser (BYOB) Team
+http://buildyourownbrowser.com
