d887e1eaeba9f93721a3439bc6f789f8ca171b43
Leslie Michael Orchard <lorchard@mozilla.com>
2010-03-24 14:50:55 -0400

    Bug 538888: Missing file for bookmark folders

---
+++ application/views/repacks/ini/bookmarks.php
@@ -0,0 +1,23 @@
+<?php
+    $sub_folders = array();
+?>
+
+[Bookmarks<?= $set_id ?>]
+<?php foreach ($bookmarks as $idx=>$bookmark): ?>
+<?php View::factory('repacks/ini/bookmark', array(
+    'idx' => $idx+1, 'bookmark' => $bookmark
+))->render(TRUE); ?>
+<?php
+    if ('folder' == $bookmark['type'] && !empty($bookmark['items'])) {
+        $sub_folders[] = View::factory('repacks/ini/bookmarks', array(
+            'set_id' => "Folder-{$bookmark['id']}",
+            'bookmarks' => $bookmark['items']
+        ))->render(FALSE);
+    }
+?>
+<?php endforeach ?>
+<?php 
+    foreach ($sub_folders as $sub_folder) {
+        echo $sub_folder;
+    }
+?>
