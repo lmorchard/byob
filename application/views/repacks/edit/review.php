60af711afe915f74872c87d880464e87305d1dd0
Leslie Michael Orchard <lorchard@mozilla.com>
2010-03-25 14:43:35 -0400

    Bug 555004: Bugfix for case where repack addon array is null

--- application/views/repacks/edit/review.php
+++ application/views/repacks/edit/review.php
@@ -38,7 +38,7 @@ $edit_base = $repack->url() . ';edit?section=';
 
             <li class="section personas clearfix">
                 <h3>Personas <a target="_top" href="<?=$edit_base?>addons">edit</a></h3>
-                <?php if (in_array('10900', $repack->addons)): ?>
+                <?php if ($repack->addons && in_array('10900', $repack->addons)): ?>
                     <p>Persona URL: <a href="<?=html::specialchars($repack->persona_url)?>" target="_new"><?=html::specialchars($repack->persona_url)?></a></p>
                     <p><span style="color: green">&#x2714;</span> The Personas add-on will be installed.</p>
                 <?php else: ?>
