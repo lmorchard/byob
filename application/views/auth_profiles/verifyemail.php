cc3c23226c9d388510e84efa1f939feec72f014b
Leslie Michael Orchard <lorchard@mozilla.com>
2010-06-01 13:33:13 -0400

    bug 567700: Add call-to-action on Account Verification Page

--- application/views/auth_profiles/verifyemail.php
+++ application/views/auth_profiles/verifyemail.php
@@ -1,4 +1,4 @@
-<?php if (!empty($invalid_token)): ?>
+<?php if (false && !empty($invalid_token)): ?>
     <p>Invalid email verification token.</p>
 <?php else: ?>
     <?php slot::set('head_title', 'account verification successful'); ?>
@@ -10,5 +10,13 @@
         application, so if you're using a computer that isn't always under your 
         control, you should log out when you're finished using BYOB by using the 
         "logout" link in the upper-right corner of every page.
+    </p>
+    <p>
+        <br />
+        <?php
+            $profile_url = url::base() .
+                "profiles/".authprofiles::get_profile('screen_name');
+        ?>
+        <a href="<?=$profile_url?>" class="button yellow large">Get Started Building Your Browser Now!</a>
     </p>
 <?php endif ?>
