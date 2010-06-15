8e7ac9f1d551bc6b9afa8c75d711112c72ab8ecb
Leslie Michael Orchard <lorchard@mozilla.com>
2010-06-15 13:53:44 -0400

    bug 572005: Fixing overzealous config defaults cleanup

--- application/modules/addon_management/config/addon_management.php
+++ application/modules/addon_management/config/addon_management.php
@@ -2,6 +2,17 @@
 /**
  * Configuration for addons available for use in repacks
  */
+$config['api_url'] = 
+    'https://services.addons.mozilla.org/en-US/firefox/api/1.3/addon/%s';
+$config['dir'] = 
+    dirname(APPPATH) . "/addons";
+$config['collections_api_url'] =
+    'https://addons.mozilla.org/en-US/firefox/api/1.3/sharing/';
+$config['collections_username'] =
+    'USERNAME_NEEDS_LOCAL_CONFIG';
+$config['collections_password'] =
+    'PASSWORD_NEEDS_LOCAL_CONFIG';
+
 $config['popular_extension_ids'] = array(
     '13661',
     '9924',
