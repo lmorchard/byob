03b0399673dfbcb868cbc74c50f7822d90248003
Leslie Michael Orchard <lorchard@mozilla.com>
2010-06-15 11:46:39 -0400

    bug 571204: Simplifying role selection for user profiles

--- application/views/auth_profiles/editprofile.php
+++ application/views/auth_profiles/editprofile.php
@@ -6,29 +6,23 @@
 <?php slot::end() ?>
 
 <?php slot::start('roles') ?><?php if ($profile->checkPrivilege('edit_roles')): ?>
-<div class="listbuilder">
+<?php
+    $checked_roles = array();
+    foreach ($roles as $role) {
+        $checked_roles[] = $role->name;
+    }
+?>
+<div>
     <ul class="list roles">
-        <?php if (!empty($roles)) foreach ($roles as $role): ?>
-            <li class="role">
-                <input type="hidden" name="roles[]" value="<?= html::specialchars($role->name) ?>" />
-                <a href="#" class="delete">[x]</a>
-                <span><?= html::specialchars( @$role_choices[$role->name] ) ?></span>
+        <?php foreach ($role_choices as $role=>$label): ?>
+            <?php if (in_array($role, array('guest','member'))) { continue; } ?>
+            <li class="field checkbox">
+                <label><?=html::specialchars($label)?></label>
+                <input type="checkbox" name="roles[]" value="<?=html::specialchars($role)?>" 
+                    <?= in_array($role, $checked_roles) ? 'checked="checked"' : '' ?> />
             </li>
         <?php endforeach ?>
-        <li class="role template">
-            <input type="hidden" name="roles[]" value="" />
-            <a href="#" class="delete">[x]</a>
-            <span></span>
-        </li>
     </ul>
-    <div class="roles-add">
-        <select class="choices" name="role_choices">
-            <?php foreach ($role_choices as $role=>$label): ?>
-                <option value="<?= html::specialchars($role) ?>"><?= html::specialchars($label) ?></option>
-            <?php endforeach ?>
-        </select>
-        <a href="#" class="add">+ add role</a>
-    </div>
 </div>
 <?php endif ?><?php slot::end() ?>
 
@@ -84,7 +78,7 @@
             View::factory('auth_profiles/elements/countries')->render(),
         )),
         (!$profile->checkPrivilege('edit_roles')) ? '' :
-            form::fieldset('Roles', array(), array( slot::get('roles') )),
+            form::fieldset('Additional Roles', array(), array( slot::get('roles') )),
         form::fieldset('finish', array('class'=>'finish'), array(
             form::field('submit_button', 'details', null, array('button_params'=>array('class'=>'button yellow required'),'class'=>'required','value'=>'Update')),
         ))
