17952c2fa7acb6eeaecad44ad8bdd49d4cc4c0c1
Leslie Michael Orchard <lorchard@mozilla.com>
2010-06-24 16:40:49 -0400

    bug 545307: Taking a stab at rearranging the home page

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,86 +1,132 @@
+<?php
+    if (authprofiles::is_logged_in()) {
+        $create_url = url::base() . "profiles/" . 
+            authprofiles::get_profile('screen_name');
+    } else {
+        $create_url = url::base().'register';
+    }
+?>
 <?php slot::set('head_title', 'home') ?>
 <?php slot::set('crumbs', 'home') ?>
 
-<div class="page_intro">
-    <div class="page_intro_content">
-        <h3><span>Customize Firefox</span></h3>
+<div class="page_intro clearfix">
 
-        <img src="<?=url::base() . 'img/home-page-intro-screenshot.jpg'?>" 
-            alt="Example Firefox screenshot"
-            class="screenshot" width="444" height="307" />
-
+    <div class="blurb">
+        <h3>Make Firefox Yours</h3>
         <p>
-            Welcome to Build Your Own Browser (BYOB), the Mozilla web 
-            application that lets you create and distribute customized versions 
-            of Firefox.
+            <em>Build Your Own Browser (BYOB)</em> is a simple way
+            that your organization can create and distribute a
+            customized version of Firefox
         </p>
-
-        <p>
-            Customizations you can make with BYOB include:
+        <a class="create_browser yellow button" href="<?=$create_url?>">
+            <span class="first_line"><strong>Create Your Browser</strong> (Free!)</span>
+            <span class="second_line">It only takes a minute.</span>
+        </a>
+        <p class="faq">
+            Got Questions?
+            <a href="faq">Check out our FAQ</a>
         </p>
-
-        <ul>
-            <li>a browser skin/theme using a bundled <a target="_new" href="http://getpersonas.com">Persona</a></li>
-            <li>pre-populated bookmarks and RSS feeds</li>
-            <li>links to a <a target="_new" href="https://addons.mozilla.org/en-US/firefox/collections/">Collection</a> of your recommended add-ons </li>
-            <li>multiple locales for Windows, OSX, and Linux</li>
-        </ul>
-
-        <h4 class="get_started">
-            <?php if (authprofiles::is_logged_in()): ?>
-                <?php
-                    $profile_url = url::base() .
-                        "profiles/".authprofiles::get_profile('screen_name');
-                ?>
-                <a class="button yellow large" href="<?=$profile_url?>">Get Started!</a>
-            <?php else: ?>
-                <a class="button yellow large" href="<?= url::base().'register' ?>">Get Started!</a>
-            <?php endif ?>
-        </h4>
 
     </div>
+
+    <div class="video">
+        <img src="img/page-intro-video.png" />
+    </div>
+
 </div>
 
-<div class="sidebar">
-    <a href="http://labs.mozilla.com/contests/extendfirefox3.5/"><img src="<?=url::base() . 'img/EXTEND_160x280.png'?>" 
-        alt="Extend Firefox 3.5: Make the next great user experience!"
-        width="160" height="280" /></a>
-</div>
+<div class="benefits clearfix">
 
-<div class="recent_browsers white_box">
-    <div class="header"><h3>Recently created browsers</h3></div>
-
-    <ul class="browsers">
-        <?php foreach ($latest_repacks as $idx=>$repack): ?>
-            <?php
-                $h = html::escape_array(array(
-                    'url'         => $repack->url,
-                    'title'       => $repack->title,
-                    'description' => $repack->description,
-                    'screen_name' => $repack->profile->screen_name,
-                    'first_name'  => $repack->profile->first_name,
-                    'last_name'   => $repack->profile->last_name,
-                    'modified'    => 
-                        date('M d Y', strtotime($repack->modified)),
-                    'profile_url' =>
-                        url::base() . 'profiles/' . $repack->profile->screen_name,
-                ));
-            ?>
-                <li class="browser <?= ($idx%2)==0 ? 'even' : 'odd' ?>">
-                <h4><a href="<?=$h['url']?>"><?=$h['title']?></a></h4>
-                <p class="meta">
-                    <span class="byline">
-                        created by 
-                        <a href="<?=$h['profile_url']?>"><?=$h['first_name'] . ' ' . $h['last_name']?></a>
-                    </span>
-                    <span class="modified"><?=$h['modified']?></span>
-                </p>
-                <div class="description <?= empty($h['description']) ? 'empty' : '' ?>">
-                    <h5 class="download"><a class="button yellow" href="<?=$h['url']?>#download">Download</a></h5>
-                    <p><?=$h['description']?></p>
-                </div>
+    <div class="your_benefits">
+        <h3>How can BYOB benefit your organization?</h3>
+        <ul>
+            <li><h4>By connecting your affinity group to you.</h4>
+                <p>Complement your online presence with a customized version of Firefox,
+                    connecting the people you serve with the information they need.</p>
             </li>
-        <?php endforeach ?>
-    </ul>
+            <li><h4>By letting you show your stripes.</h4>
+                <p>Add your identity to Firefox with a Persona, bookmarks, and add-ons.</p>
+            </li>
+            <li><h4>It supports multiple languages and operating systems.</h4>
+                <p>Your version of Firefox is available in over 70 languages on Linux,
+                    Mac, and Windows. No one's left behind.</p>
+            </li>
+            <li><h4>It's free to use and distribute.</h4>
+                <p>We provide the tools for you to create your own browser with no 
+                    development, maintenance, or hosting costs.</p>
+            </li>
+        </ul>
+    </div>
 
+    <div class="who_benefits">
+        <h3>Who can this help?</h3>
+        <ul>
+            <li><h4>Organizations.</h4>
+                <p>Highlighting a product, service or information about your 
+                    offerings? Use BYOB to create a browser that connects your users 
+                    to you out of the box.</p>
+            </li>
+            <li><h4>Social Groups.</h4>
+                <p>Want people to know what useful sources and sites are 
+                    available for your running group, gaming clan, club, or other 
+                    like-minded people? Share them with a version of Firefox that's
+                    customized for your common interests.</p>
+            </li>
+            <li><h4>Friends and Family.</h4>
+                <p>Want to easily share the best of the web with the people you care 
+                    about? Give them a browser that puts everything a clich away, without
+                    them having to hunt for it.</p>
+            </li>
+        </ul>
+    </div>
+
+</div>
+
+<div class="showcase clearfix">
+
+    <div class="recent_browsers">
+        <h3>What browsers are others making?</h3>
+
+        <ul class="browsers">
+            <?php foreach ($latest_repacks as $idx=>$repack): ?>
+                <?php
+                    $h = html::escape_array(array(
+                        'url'         => $repack->url,
+                        'title'       => $repack->title,
+                        'description' => $repack->description,
+                        'screen_name' => $repack->profile->screen_name,
+                        'first_name'  => $repack->profile->first_name,
+                        'last_name'   => $repack->profile->last_name,
+                        'modified'    => 
+                            date('D, M d', strtotime($repack->modified)),
+                        'profile_url' =>
+                            url::base() . 'profiles/' . $repack->profile->screen_name,
+                    ));
+                ?>
+                    <li class="browser <?= ($idx%2)==0 ? 'even' : 'odd' ?>">
+                    <h4><a href="<?=$h['url']?>"><?=$h['title']?></a></h4>
+                    <p class="meta">
+                        <span class="byline">
+                            Created by 
+                            <a href="<?=$h['profile_url']?>"><?=$h['first_name'] . ' ' . $h['last_name']?></a>
+                        </span>
+                        <span class="modified">on <?=$h['modified']?></span>
+                    </p>
+                </li>
+            <?php endforeach ?>
+        </ul>
+
+    </div>
+
+    <div class="whos_using">
+    </div>
+
+</div>
+
+<div class="action_call_footer">
+    <h3>Ready to start building a browser of your own?</h3>
+    <a class="create_browser yellow button" href="<?=$create_url?>">
+        <span class="first_line"><strong>Create Your Browser</strong> (Free!)</span>
+        <span class="second_line">It only takes a minute.</span>
+    </a>
 </div>
