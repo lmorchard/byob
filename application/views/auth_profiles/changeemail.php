07d456a6aa91e02419f8c4febce09e7171721ee9
Leslie Michael Orchard <lorchard@mozilla.com>
2010-08-06 09:36:32 -0400

    bug 503328: missed a few more strings for l10n markup

--- application/views/auth_profiles/changeemail.php
+++ application/views/auth_profiles/changeemail.php
@@ -1,17 +1,15 @@
 <?php if (!empty($email_verification_token_set)): ?>
-    <p>
-        Check your inbox for a link to verify this new email address.
+    <p><?=_('Check your inbox for a link to verify this new email address.')?>
     </p>
 <?php else: ?>
     <?php form::$errors = @$form_errors ?>
     <?= 
     form::build(null, array('class'=>'changeemail'), array(
-        form::fieldset('Change email', null, array(
-            form::field('input', 'current_email', 'Current email address', array('value'=>authprofiles::get_login('email'), 'disabled'=>'true')),
-            form::field('input', 'new_email', 'New email address', array('class'=>'required')),
-            form::field('submit_button', 'change', null, array('button_params'=>array('class'=>'button yellow required'),'value'=>'Change email'))
+        form::fieldset(_('Change email'), null, array(
+            form::field('input', 'current_email', _('Current email address'), array('value'=>authprofiles::get_login('email'), 'disabled'=>'true')),
+            form::field('input', 'new_email', _('New email address'), array('class'=>'required')),
+            form::field('submit_button', 'change', null, array('button_params'=>array('class'=>'button yellow required'),'value'=>_('Change email')))
         ))
     )) 
     ?>
 <?php endif ?>
-
--- application/views/auth_profiles/login.php
+++ application/views/auth_profiles/login.php
@@ -14,13 +14,13 @@
 
 <?php if ($is_popup): ?>
     <div class="header">
-        <h2>Log into your account</h2>
+        <h2><?=_('Log into your account')?></h2>
     </div>
     <div class="content">
 <?php endif ?>
 
 <?php if (!empty($login_inactive)): ?>
-    <p>Sorry, that login has been disabled.</p>
+    <p><?=_('Sorry, that login has been disabled.')?></p>
 <?php endif ?>
 
 <?php if (!empty($no_verified_email)): ?>
@@ -30,17 +30,9 @@
 <?php endif ?>
 <?php if (!empty($_GET['show_reverify'])): ?>
     <div class="warning">
-        <p>
-            The account you are attempting to login with has not been activated. In order
-            to use BYOB, you must first activate your account by following the instructions
-            that were sent to the email address associated with that account. If you did
-            not receive this email, please ensure any anti-spam methods will accept mail
-            from byob-notify-noreply@mozilla.com and use the 
-            "Re-send Account Activation Information" button below. If
-            you still do not receive the activation information, please contact us.
-        </p>
+        <p><?=_('The account you are attempting to login with has not been activated. In order to use BYOB, you must first activate your account by following the instructions that were sent to the email address associated with that account. If you did not receive this email, please ensure any anti-spam methods will accept mail from byob-notify-noreply@mozilla.com and use the "Re-send Account Activation Information" button below. If you still do not receive the activation information, please contact us.')?></p>
         <form action="<?=url::site('reverifyemail/'.urlencode($_GET['show_reverify']))?>" method="POST">
-            <button class="button blue large" type="submit">Re-send Account Activation Information</button>
+            <button class="button blue large" type="submit"><?=_('Re-send Account Activation Information')?></button>
         </form>
     </div>
 <?php endif ?>
@@ -55,12 +47,12 @@ form::$errors = array();
 <li class="required submit">
     <label class="hidden" for="login"/>
     <ul class="other">
-        <li><a target="_top" href="<?=url::site('forgotpassword')?>">Forgot your password?</a></li>
-        <li><a target="_top" href="<?=url::site('register')?>">Need an account?</a></li>
+        <li><a target="_top" href="<?=url::site('forgotpassword')?>"><?=_('Forgot your password?')?></a></li>
+        <li><a target="_top" href="<?=url::site('register')?>"><?=_('Need an account?')?></a></li>
     </ul>
-    <button id="login" class="submit required button large yellow">Login</button>
+    <button id="login" class="submit required button large yellow"><?=_('Login')?></button>
     <?php if ($is_popup): ?>
-        <button id="cancel" class="popup_cancel button large blue">Cancel</button>
+        <button id="cancel" class="popup_cancel button large blue"><?=_('Cancel')?></button>
     <?php endif ?>
 </li>
 <?php slot::end() ?>
@@ -69,10 +61,10 @@ form::$errors = array();
 form::build(url::site('login'), array('class'=>'login'), array(
     form::field('hidden', 'jump', ''),
     form::fieldset(null, array('class'=>'login'), array(
-        form::field('input',    'login_name', 'Username', array(
+        form::field('input',    'login_name', _('Username'), array(
             'class'=>'required' . ( $invalid ? ' error' : '') 
         )),
-        form::field('password', 'password', 'Password', array(
+        form::field('password', 'password', _('Password'), array(
             'class'=>'required' . ( $invalid ? ' error' : '') 
         )),
         slot::get('submit')
