f640d3d7f25bedb9cf365f69750892c7815deb28
Leslie Michael Orchard <lorchard@mozilla.com>
2010-10-05 09:43:13 -0400

    Bug 601620 - Replace existing add-on installer with MPL-licensed version

--- application/modules/addon_management/repack_assets/distribution/bundles/{04C927C9-E491-4DDC-9D45-54C145422A15}/resources/installerService.js
+++ application/modules/addon_management/repack_assets/distribution/bundles/{04C927C9-E491-4DDC-9D45-54C145422A15}/resources/installerService.js
@@ -1,6 +1,38 @@
-/**
- * Copyright (c) 2010 Appcoast Ltd. All rights reserved.
- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Add-on Installer.
+ *
+ * The Initial Developer of the Original Code is Appcoast.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Andres Hernandez <andres@appcoast.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 var EXPORTED_SYMBOLS = [];
 
@@ -26,6 +58,10 @@ AddonInstaller.InstallerService = {
   _MAX_RETRY_PREFKEY : AddonInstaller.PrefBranch + "retry.maxRestarts",
   /* Stores the current number of restarts in case of error. */
   _CUR_RETRY_PREFKEY : AddonInstaller.PrefBranch + "retry.curRestarts",
+  /* Flag to show the loading the dialog. */
+  _SHOW_LOADING_PREFKEY : AddonInstaller.PrefBranch + "show.loadingDialog",
+  /* Flag to show the selection the dialog. */
+  _SHOW_SELECTION_PREFKEY : AddonInstaller.PrefBranch + "show.selectionDialog",
 
   /* Logger for this object. */
   _logger : null,
@@ -55,9 +91,6 @@ AddonInstaller.InstallerService = {
       Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
     this._xulRuntime =
       Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime);
-
-    AddonInstaller.ObserverService.addObserver(
-      this, "profile-change-teardown", false);
   },
 
   /**
@@ -70,6 +103,8 @@ AddonInstaller.InstallerService = {
       AddonInstaller.Application.prefs.get(this._PREVENT_PREFKEY);
 
     if (preventPref && preventPref.value) {
+      let that = this;
+      let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
       let newAddonsPref =
         AddonInstaller.Application.prefs.get("extensions.newAddons");
       let warnOnRestart =
@@ -87,19 +122,53 @@ AddonInstaller.InstallerService = {
           "browser.warnOnRestart", warnOnRestart.value);
         this._preferenceService.clearUserPref(this._WARNONRESTART_PREFKEY);
       }
-      if (defaultBrowser) {
-        AddonInstaller.Application.prefs.setValue(
-          "browser.shell.checkDefaultBrowser", defaultBrowser.value);
-        this._preferenceService.clearUserPref(this._DEFAULTBROWSER_PREFKEY);
-      }
+
+      timer.initWithCallback(
+        { notify : function() { that._selectFirstRunTab(); } },
+        1500, Ci.nsITimer.TYPE_ONE_SHOT);
     }
   },
 
   /**
-   * Starts the install process.
+   * Re-selects the first run tab after opening the main window.
    */
-  startInstallProcess : function() {
-    this._logger.debug("startInstallProcess");
+  _selectFirstRunTab : function() {
+    this._logger.trace("_selectFirstRunTab");
+
+    let defaultBrowser =
+      AddonInstaller.Application.prefs.get(this._DEFAULTBROWSER_PREFKEY);
+    let formatter =
+      Cc["@mozilla.org/toolkit/URLFormatterService;1"].
+        getService(Ci.nsIURLFormatter);
+    let windowMediator =
+      Cc["@mozilla.org/appshell/window-mediator;1"].
+        getService(Ci.nsIWindowMediator);
+    let win = windowMediator.getMostRecentWindow("navigator:browser");
+    let firstRunPage = formatter.formatURLPref("startup.homepage_welcome_url");
+    let tabs = win.gBrowser.tabContainer.childNodes;
+    let tab = null;
+
+    for (let i = 0; i < tabs.length; i++) {
+      tab = tabs[i];
+
+      if (firstRunPage == tab.linkedBrowser.currentURI.spec) {
+        win.gBrowser.selectedTab = tab;
+        break;
+      }
+    }
+
+    if (defaultBrowser) {
+      AddonInstaller.Application.prefs.setValue(
+        "browser.shell.checkDefaultBrowser", defaultBrowser.value);
+      this._preferenceService.clearUserPref(this._DEFAULTBROWSER_PREFKEY);
+    }
+  },
+
+  /**
+   * Check if the install process should start.
+   */
+  checkInstallProcess : function() {
+    this._logger.debug("checkInstallProcess");
 
     let globalPreference =
       AddonInstaller.Application.prefs.get(this._GLOBLAL_PREFKEY);
@@ -111,9 +180,38 @@ AddonInstaller.InstallerService = {
     let curRetry = (curRetryPref ? curRetryPref.value : 0);
 
     if (globalPreference && !globalPreference.value && curRetry < maxRetry) {
-      this._openLoadingWindow();
-      this._installExtensions();
+      this._startInstallProcess();
     }
+  },
+
+  /**
+   * Starts the install process.
+   */
+  _startInstallProcess : function() {
+    this._logger.trace("_startInstallProcess");
+
+    let showSelectionDialogPref =
+      AddonInstaller.Application.prefs.get(this._SHOW_SELECTION_PREFKEY);
+    let showLoadingDialogPref =
+      AddonInstaller.Application.prefs.get(this._SHOW_LOADING_PREFKEY);
+    let defaultBrowserPref =
+      AddonInstaller.Application.prefs.get("browser.shell.checkDefaultBrowser");
+    let extensionArray = this._getExtensionsToInstall();
+
+    if (defaultBrowserPref && defaultBrowserPref.value) {
+      AddonInstaller.Application.prefs.setValue(
+        this._DEFAULTBROWSER_PREFKEY, defaultBrowserPref.value);
+      AddonInstaller.Application.prefs.setValue(
+        "browser.shell.checkDefaultBrowser", false);
+    }
+
+    if (showSelectionDialogPref && showSelectionDialogPref.value) {
+      extensionArray = this._openSelectionWindow(extensionArray);
+    }
+    if (showLoadingDialogPref && showLoadingDialogPref.value) {
+      this._openLoadingWindow();
+    }
+    this._installExtensions(extensionArray);
   },
 
   /**
@@ -134,21 +232,41 @@ AddonInstaller.InstallerService = {
   },
 
   /**
-   * Installs the extensions.
+   * Opens the selection window.
+   * @param aExtensionList the extension list.
    */
-  _installExtensions : function() {
+  _openSelectionWindow : function(aExtensionList) {
+    this._logger.trace("_openSelectionWindow");
+
+    let windowWatcher =
+      Cc["@mozilla.org/embedcomp/window-watcher;1"].
+        getService(Ci.nsIWindowWatcher);
+    let selectionDialog = windowWatcher.openWindow(
+      null,"chrome://addoninstaller/content/selection.xul",
+      "addon-installer-selection-dialog",
+      "chrome,modal,centerscreen,resizable=no", aExtensionList);
+
+    return aExtensionList;
+  },
+
+  /**
+   * Installs the extensions.
+   * @param aExtensionList the extension list.
+   */
+  _installExtensions : function(aExtensionList) {
     this._logger.trace("_installExtensions");
 
-    let extensionArray = this._getExtensionsToInstall();
     let extensionInstalled = null;
     let allExtensionsInstalled = true;
 
     try {
-      for (let i = 0; i < extensionArray.length; i++) {
-        extensionInstalled = this._installExtension(extensionArray[i]);
+      for (let i = 0; i < aExtensionList.length; i++) {
+        if (aExtensionList[i].install) {
+          extensionInstalled = this._installExtension(aExtensionList[i]);
 
-        if (!extensionInstalled) {
-          allExtensionsInstalled = false;
+          if (!extensionInstalled) {
+            allExtensionsInstalled = false;
+          }
         }
       }
 
@@ -171,6 +289,8 @@ AddonInstaller.InstallerService = {
         }
       }
 
+      AddonInstaller.ObserverService.addObserver(
+        this, "profile-change-teardown", false);
       this._setRestartPreferences();
       this._restartFirefox();
     } catch (e) {
@@ -325,8 +445,14 @@ AddonInstaller.InstallerService = {
 
       while (sections.hasMore()) {
         section = sections.getNext();
-        extInfo = {};
+        extInfo = { install : true };
 
+        try {
+          extInfo.name = iniParser.getString(section,"name");
+        } catch (e) {
+          extInfo.name = "";
+          this._logger.error("_getExtensionsToInstall:\n" + e);
+        }
         try {
           extInfo.id = iniParser.getString(section,"id");
         } catch (e) {
@@ -370,6 +496,8 @@ AddonInstaller.InstallerService = {
       this._logger.error("_getExtensionsToInstall: config.ini not found!");
     }
 
+    extensions.wrappedJSObject = extensions;
+
     return extensions;
   },
 
@@ -381,17 +509,11 @@ AddonInstaller.InstallerService = {
 
     let warnOnRestart =
       AddonInstaller.Application.prefs.get("browser.warnOnRestart").value;
-    let defaultBrowser =
-      AddonInstaller.Application.prefs.get(
-        "browser.shell.checkDefaultBrowser").value;
 
-    AddonInstaller.Application.prefs.setValue(this._PREVENT_PREFKEY, true);
     AddonInstaller.Application.prefs.setValue(
       this._WARNONRESTART_PREFKEY, warnOnRestart);
-    AddonInstaller.Application.prefs.setValue(
-      this._DEFAULTBROWSER_PREFKEY, defaultBrowser);
     AddonInstaller.Application.prefs.setValue("browser.warnOnRestart", false);
-    AddonInstaller.Application.prefs.setValue("browser.shell.checkDefaultBrowser", false);
+    AddonInstaller.Application.prefs.setValue(this._PREVENT_PREFKEY, true);
   },
 
   /**
