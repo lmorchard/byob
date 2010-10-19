8d2482c150b3431640e4a2249dfdc3d34692c77e
Leslie Michael Orchard <lorchard@mozilla.com>
2010-10-19 17:22:40 -0400

    bug 603590: redirect from docroot calls http, not https, breaking login

---
+++ application/hooks/correct_redirect_http_proto.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The url::redirect() helper uses the HTTP(S) attempted by the client when 
+ * composing an absolute URL for a redirect. This hook changes that to the 
+ * HTTP protocol configured for the site.
+ *
+ * @package    Mozilla_BYOB
+ * @subpackage hooks
+ * @author     l.m.orchard <lorchard@mozilla.com>
+ */
+class Mozilla_BYOB_CorrectRedirectHttpProto
+{
+    function correct_redirect_proto() {
+        $uri = Event::$data;
+        if (strpos($uri, '://') === FALSE) {
+            $uri = url::site($uri, Kohana::config('core.site_protocol'));
+        }
+        Event::$data = $uri;
+    }
+}
+Event::add('system.redirect', 
+    array('Mozilla_BYOB_CorrectRedirectHttpProto', 'correct_redirect_proto'));
